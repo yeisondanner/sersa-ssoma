0a8f8b8c1a9610e8c0e8a4f8a943211991786c78
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-09-01 12:55:01 -0500

    creacion de la interfaz

---
+++ Controllers/Macroprocess.php
@@ -0,0 +1,30 @@
+<?php
+
+class Macroprocess extends Controllers
+{
+    /**
+     * Constructor de la clase
+     */
+    public function __construct()
+    {
+        isSession();
+        parent::__construct();
+    }
+    /**
+     * Funcion que devuelve la vista de la gestion de usuarios
+     * @return void
+     */
+    public function macroprocess()
+    {
+        $data['page_id'] = 12;
+        permissionInterface($data['page_id']);
+        $data['page_title'] = "Gestión de Macroprocesos";
+        $data['page_description'] = "Permite gestionar los macroprocesos del sistema GSSOHMA.";
+        $data['page_container'] = "Macroprocess";
+        $data['page_view'] = 'macroprocess';
+        $data['page_js_css'] = "macroprocess";
+        $data['page_vars'] = ["login", "login_info", "lastConsult"];
+        registerLog("Información de navegación", "El usuario entro a: " . $data['page_title'], 3, $_SESSION['login_info']['idUser']);
+        $this->views->getView($this, "macroprocess", $data);
+    }
+}
