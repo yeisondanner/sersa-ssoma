f69c711996b1ffeeec8b3ad9abbb288cbc84422a
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-09-04 18:03:59 -0500

    vista de usuarios

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -21,11 +21,20 @@ class Dashboard extends Controllers
                 echo "<script>window.location.href='" . base_url() . "/errors/notfound" . "';</script>";
                 die();
             }
+            //obtencion de informacion generales
+            $idMacroprocess = $arrayIds[0];
+            $dataMacroprocess = $this->model->select_macroprocess_by_id($idMacroprocess);
             //validamos si el array solo es logitud de 1 quiere decir que estamos dentro de un macroprocess
             if (count($arrayIds) == 1) {
-                $idMacroprocess = $arrayIds[0];
-                $dataMacroprocess = $this->model->select_macroprocess_by_id($idMacroprocess);
-                $function = $dataProcess = $this->proceses_associed_macroprocess_by_id($idMacroprocess, $dataMacroprocess);
+                $function = $this->proceses_associed_macroprocess_by_id($idMacroprocess, $dataMacroprocess);
+            } else if (count($arrayIds) == 2) {
+                $idProcess = $arrayIds[1];
+                $dataProcess = ["Macroprocess" => $dataMacroprocess, "Process" => $this->model->select_process_by_id($idProcess)];
+                $function = $this->thread_associed_process_by_id($idProcess, $dataProcess);
+            } else {
+                $idProcess = $arrayIds[1];
+                $dataProcess = ["Macroprocess" => $dataMacroprocess, "Process" => $this->model->select_process_by_id($idProcess)];
+                $function = $this->subthread_by_thread_by_process_by_macroprocess($arrayIds, $dataProcess);
             }
         } else {
             $function = $this->index();
@@ -118,13 +127,11 @@ class Dashboard extends Controllers
      * @param  int $id
      * @return string
      */
-    public function proceses_associed_macroprocess_by_id($id, array $data)
+    public function proceses_associed_macroprocess_by_id(int $id, array $data)
     {
-        $dataProcess = $this->model->select_process_by_id($id);
+        $dataProcess = $this->model->select_process_associed_macroprocess_by_id($id);
         $html = "";
         $html .= '
-
-
         <div class="app-title pt-5">
             <div class="w-100">
                 <h1 class="text-primary mb-3"><i class="fa fa-university"></i>' . $data["mp_name"] . '</h1>
@@ -132,9 +139,9 @@ class Dashboard extends Controllers
                 <hr class="w-100">
                     <ul class="app-breadcrumb breadcrumb bg-primary text-white p-2">
                         <li class="breadcrumb-item"><a
-                                href="' . base_url() . '/dashboard/" class="text-white"><i class="fa fa fa-university fa-lg"></i></a></li>
+                                href="' . base_url() . '/dashboard/" class="text-white"><i class="fa fa fa-globe fa-lg"></i></a></li>
                         <li class="breadcrumb-item"><a
-                                href="' . base_url() . '/dashboard/dashboard/' . $data["idMacroprocess"] . '" class="text-white">' . $data["mp_name"] . '</a></li>
+                                href="' . base_url() . '/dashboard/dashboard/' . $data["idMacroprocess"] . '" class="text-white"><i class="fa fa fa-university"></i> ' . $data["mp_name"] . '</a></li>
                     </ul>
             </div>          
         
@@ -147,7 +154,7 @@ class Dashboard extends Controllers
             $html .= '
                 <!-- Card 1 -->
                 <div class="col-md-4 mb-4">
-                    <a href="' . base_url() . '/dashboard/dashboard/' . $v["idProcess"] . '"
+                    <a href="' . base_url() . '/dashboard/dashboard/' . $data["idMacroprocess"] . '/' . $v["idProcess"] . '"
                         class="card custom-card p-4 text-center h-100" data-toggle="tooltip" data-placement="top"
                         title="Haz clic para ver más sobre ' . $v["p_name"] . '">
                         <div class="icon-wrapper bg-primary mx-auto">
@@ -176,7 +183,7 @@ class Dashboard extends Controllers
             </button>';
         }
         $btnRight = '';
-        if ($position < (count($arrayMacroprocess)-1)) {
+        if ($position < (count($arrayMacroprocess) - 1)) {
             $btnRight = '   <!-- Botón Siguiente -->
             <button class="btn btn-primary" title="Siguiente - ' . $arrayMacroprocess[($position + 1)]['mp_name'] . '" onclick="window.location.href=`' . base_url() . '/dashboard/dashboard/' . $arrayMacroprocess[($position + 1)]['idMacroprocess'] . '`">
                 <i class="fa fa-arrow-right"></i>
@@ -207,5 +214,236 @@ class Dashboard extends Controllers
         </script>';
         return $html;
     }
+    /**
+     * Metodo que se encarga de mostrar los subprocesos asociados a los procesos que estan
+     * @param int $idprocess
+     * @return mixed
+     */
+    public function thread_associed_process_by_id(int $idprocess, array $data)
+    {
+        $idMacroprocess = $data['Macroprocess']["idMacroprocess"];
+        $dataThread = $this->model->select_thread_associed_process_associed_macroprocess_by_id($idprocess, $idMacroprocess);
+        $html = "";
+        //elemento cabezera
+        $html .= '
+        <div class="app-title pt-5">
+            <div class="w-100">
+                <h1 class="text-primary mb-3"><i class="fa fa-university"></i>' . $data['Process']["p_name"] . '</h1>
+                <p class="mb-2">' . $data['Process']["p_description"] . '</p>
+                <hr class="w-100">
+                    <ul class="app-breadcrumb breadcrumb bg-primary text-white p-2">
+                        <li class="breadcrumb-item"><a
+                                href="' . base_url() . '/dashboard/" class="text-white"><i class="fa fa fa-globe fa-lg"></i></a></li>
+                                <li class="breadcrumb-item"><a
+                                href="' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '" class="text-white"><i class="fa fa fa-university"></i> ' . $data['Macroprocess']["mp_name"] . '</a></li>
+                        <li class="breadcrumb-item"><a
+                                href="' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $data['Process']["idProcess"] . '" class="text-white"><i class="fa fa fa-bookmark"></i> ' . $data['Process']["p_name"] . '</a></li>
+                    </ul>
+            </div>          
+        
+        </div>';
+        //elemento contenido
+        $html .= ' <!--Listado de los macroprocesos-->
+        <div class="row">';
+        foreach ($dataThread as $k => $v):
+            //validdamos el que tipo de menu es
+            if ($v['t_type'] == "open_menu") {
+                $icon = "<i class='fa fa-bars'></i>";
+            } else if ($v['t_type'] == "open_file") {
+                $icon = "<i class='fa fa-file'></i>";
 
+            } else if ($v["t_type"] == "open_form") {
+                $icon = "<i class='fa fa-pencil'></i>";
+            } else {
+                $icon = "<i class='fa fa-exclamation'></i>";
+            }
+            $html .= '
+                <!-- Card 1 -->
+                <div class="col-md-4 mb-4">
+                    <a href="' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $data['Process']["idProcess"] . '/' . $v["idThreads"] . '"
+                        class="card custom-card p-4 text-center h-100" data-toggle="tooltip" data-placement="top"
+                        title="Haz clic para ver más sobre ' . $v["t_name"] . '">
+                        <div class="icon-wrapper bg-primary mx-auto">
+                            ' . $icon . '
+                        </div>
+                        <h5>' . $v["t_name"] . '</h5>
+                        <p class="text-justify" title="' . $v["t_description"] . '">' . limitarCaracteres($v["t_description"], 50, "...") . '</p>
+                        <div class="date"><i class="fa fa-calendar"></i> ' . dateFormat($v["t_registrationDate"]) . '</div>
+                    </a>
+                </div>';
+        endforeach;
+        $html .= '</div>';
+        //metodo que se encarga de configurar los botones de navegacion
+        $arrayProcess = $this->model->select_process_associed_macroprocess_by_id($idMacroprocess);
+        $position = 0;
+        //esto se encarga de devolve la posicion de array
+        foreach ($arrayProcess as $key => $value) {
+            if ($value['idProcess'] == $idprocess) {
+                $position = $key;
+                break;
+            }
+        }
+        $btnLeft = "";
+        if ($position > 0) {
+            $btnLeft = '<!-- Botón Anterior -->
+            <button class="btn btn-primary" title="Anterior - ' . $arrayProcess[($position - 1)]['p_name'] . '" onclick="window.location.href=`' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $arrayProcess[($position - 1)]['idProcess'] . '`">
+                <i class="fa fa-arrow-left"></i>
+            </button>';
+        }
+        $btnRight = '';
+        if ($position < (count($arrayProcess) - 1)) {
+            $btnRight = '   <!-- Botón Siguiente -->
+            <button class="btn btn-primary" title="Siguiente - ' . $arrayProcess[($position + 1)]['p_name'] . '" onclick="window.location.href=`' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $arrayProcess[($position + 1)]['idProcess'] . '`">
+                <i class="fa fa-arrow-right"></i>
+            </button>';
+        }
+        $html .= '
+        <!-- Botones Flotantes -->
+        <div class="floating-buttons">
+            ' . $btnLeft . '
+            <!-- Botón Subir nivel -->
+            <button class="btn btn-warning" title="Subir un nivel" onclick="window.location.href=`' . base_url() . '/dashboard/dashboard/' . '`">
+                <i class="fa fa-arrow-up"></i>
+            </button>
+
+            <!-- Botón Recargar -->
+            <button class="btn btn-success" title="Recargar" onclick="location.reload()">
+                <i class="fa fa-refresh"></i>
+            </button>
+            ' . $btnRight . '
+        </div>
+        <!-- Activar tooltips solo en hover -->
+        <script>
+            $(function () {
+                $(`[data-toggle="tooltip"]`).tooltip({
+                    trigger: `hover`
+                })
+            })
+        </script>';
+        return $html;
+    }
+    /**
+     * Metodo que encarga de obtener los subthreas hijas
+     */
+    public function subthread_by_thread_by_process_by_macroprocess(array $arrids, array $data)
+    {
+        $idThread = $arrids[(count($arrids) - 1)];
+        $dataThread = $this->model->select_thread_id($idThread);
+        $html = '';
+        $idMacroprocess = $data['Macroprocess']["idMacroprocess"];
+        $idProcess = $data['Process']["idProcess"];
+        //elemento cabezera
+        $html .= '
+        <div class="app-title pt-5">
+            <div class="w-100">
+                <h1 class="text-primary mb-3"><i class="fa fa-tag"></i> ' . $dataThread['t_name'] . '</h1>
+                <p class="mb-2">' . $dataThread['t_description'] . '</p>
+                <hr class="w-100">
+                    <ul class="app-breadcrumb breadcrumb bg-primary text-white p-2">                    
+                     <li class="breadcrumb-item"><a
+                                href="' . base_url() . '/dashboard/" class="text-white"><i class="fa fa fa-globe fa-lg"></i></a></li>
+                                           <li class="breadcrumb-item"><a
+                                href="' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '" class="text-white"><i class="fa fa fa-university"></i> ' . $data['Macroprocess']["mp_name"] . '</a></li>
+                        <li class="breadcrumb-item"><a
+                                href="' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $data['Process']["idProcess"] . '" class="text-white"><i class="fa fa fa-bookmark"></i> ' . $data['Process']["p_name"] . '</a></li>                        
+                    
+                                ';
+        $url = base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $data['Process']["idProcess"];
+        foreach (array_slice($arrids, 2) as $key => $value) {
+            $url .= '/' . $value;
+            $infoThread = $this->model->select_thread_id($value);
+            $html .= '<li class="breadcrumb-item"><a
+                                href="' . $url . '" class="text-white"><i class="fa fa fa-tag"></i> ' . $infoThread["t_name"] . '</a></li>';
+        }
+        $html .= '</ul>
+            </div>          
+        
+        </div>';
+        //obtenemos los elementos hijo
+        $dataSubthreads = $this->model->select_subthread_associed_thread_associed_process_associed_macroprocess($idThread, $idProcess, $idMacroprocess);
+        //elemento contenido
+        $html .= ' <!--Listado de los macroprocesos-->
+        <div class="row">';
+        foreach ($dataSubthreads as $k => $v):
+            //validdamos el que tipo de menu es
+            if ($v['t_type'] == "open_menu") {
+                $icon = "<i class='fa fa-bars'></i>";
+            } else if ($v['t_type'] == "open_file") {
+                $icon = "<i class='fa fa-file'></i>";
+
+            } else if ($v["t_type"] == "open_form") {
+                $icon = "<i class='fa fa-pencil'></i>";
+            } else {
+                $icon = "<i class='fa fa-exclamation'></i>";
+            }
+            $html .= '
+                <!-- Card 1 -->
+                <div class="col-md-4 mb-4">
+                    <a href="' . $url . '/' . $v["idThreads"] . '"
+                        class="card custom-card p-4 text-center h-100" data-toggle="tooltip" data-placement="top"
+                        title="Haz clic para ver más sobre ' . $v["t_name"] . '">
+                        <div class="icon-wrapper bg-primary mx-auto">
+                            ' . $icon . '
+                        </div>
+                        <h5>' . $v["t_name"] . '</h5>
+                        <p class="text-justify" title="' . $v["t_description"] . '">' . limitarCaracteres($v["t_description"], 50, "...") . '</p>
+                        <div class="date"><i class="fa fa-calendar"></i> ' . dateFormat($v["t_registrationDate"]) . '</div>
+                    </a>
+                </div>';
+        endforeach;
+        $html .= '</div>';
+
+        /**
+         * ============================================================================ 
+         *metodo que se encarga de configurar los botones de navegacion
+         *=============================================================================
+         **/
+        $arrayProcess = $this->model->select_process_associed_macroprocess_by_id($idMacroprocess);
+        $position = 0;
+        //esto se encarga de devolve la posicion de array
+        foreach ($arrayProcess as $key => $value) {
+            if ($value['idProcess'] == $idprocess) {
+                $position = $key;
+                break;
+            }
+        }
+        $btnLeft = "";
+        if ($position > 0) {
+            $btnLeft = '<!-- Botón Anterior -->
+            <button class="btn btn-primary" title="Anterior - ' . $arrayProcess[($position - 1)]['p_name'] . '" onclick="window.location.href=`' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $arrayProcess[($position - 1)]['idProcess'] . '`">
+                <i class="fa fa-arrow-left"></i>
+            </button>';
+        }
+        $btnRight = '';
+        if ($position < (count($arrayProcess) - 1)) {
+            $btnRight = '   <!-- Botón Siguiente -->
+            <button class="btn btn-primary" title="Siguiente - ' . $arrayProcess[($position + 1)]['p_name'] . '" onclick="window.location.href=`' . base_url() . '/dashboard/dashboard/' . $idMacroprocess . '/' . $arrayProcess[($position + 1)]['idProcess'] . '`">
+                <i class="fa fa-arrow-right"></i>
+            </button>';
+        }
+        $html .= '
+        <!-- Botones Flotantes -->
+        <div class="floating-buttons">
+            ' . $btnLeft . '
+            <!-- Botón Subir nivel -->
+            <button class="btn btn-warning" title="Subir un nivel" onclick="window.location.href=`' . base_url() . '/dashboard/dashboard/' . '`">
+                <i class="fa fa-arrow-up"></i>
+            </button>
+
+            <!-- Botón Recargar -->
+            <button class="btn btn-success" title="Recargar" onclick="location.reload()">
+                <i class="fa fa-refresh"></i>
+            </button>
+            ' . $btnRight . '
+        </div>
+        <!-- Activar tooltips solo en hover -->
+        <script>
+            $(function () {
+                $(`[data-toggle="tooltip"]`).tooltip({
+                    trigger: `hover`
+                })
+            })
+        </script>';
+        return $html;
+    }
 }
--- Models/DashboardModel.php
+++ Models/DashboardModel.php
@@ -2,6 +2,8 @@
 class DashboardModel extends Mysql
 {
     private int $id;
+    private int $idprocess;
+    private int $idmacroprocess;
     public function __construct()
     {
         parent::__construct();
@@ -54,12 +56,78 @@ class DashboardModel extends Mysql
      * @param  int $id
      * @return mixed
      */
-    public function select_process_by_id(int $id)
+    public function select_process_associed_macroprocess_by_id(int $id)
     {
         $this->id = $id;
         $sql = "SELECT*FROM tb_process AS tbp WHERE tbp.macroprocess_id=? AND tbp.p_status='Activo';";
         $request = $this->select_all($sql, [$this->id]);
         return $request;
     }
-
+    /**
+     * Metodo que obtiene la informacion de los procesos
+     * @param  int $id
+     * @return mixed
+     */
+    public function select_process_by_id(int $id)
+    {
+        $this->id = $id;
+        $sql = "SELECT*FROM tb_process AS tbp WHERE tbp.idProcess=? AND tbp.p_status='Activo'";
+        $request = $this->select($sql, [$this->id]);
+        return $request;
+    }
+    /**
+     * Metodo que obtiene el thread asociado al process que no tiene otro thread padres
+     * @param int $idprocess
+     * @return mixed
+     */
+    public function select_thread_associed_process_associed_macroprocess_by_id(int $idprocess, int $idmacroprocess)
+    {
+        $this->idprocess = $idprocess;
+        $this->idmacroprocess = $idmacroprocess;
+        $sql = "SELECT tbt.*FROM tb_threads AS tbt 
+                INNER JOIN tb_process AS tbp ON tbp.idProcess=tbt.process_id
+                WHERE tbp.macroprocess_id=?  AND  tbt.process_id=? AND tbt.threads_father IS NULL AND tbt.t_status = 'Activo';";
+        $request = $this->select_all($sql, [$this->idmacroprocess, $this->idprocess]);
+        return $request;
+    }
+    /**
+     * Metodo que obtiene  informacion de Thread por us ID
+     * @param  int  $$id
+     * @return mixed
+     */
+    public function select_thread_id(int $id)
+    {
+        $this->id = $id;
+        $sql = "SELECT*FROM tb_threads AS tbt WHERE tbt.idThreads=? AND tbt.t_status='Activo'";
+        $request = $this->select($sql, [$this->id]);
+        return $request;
+    }
+    /**
+     * Metodo que se encarga de obtener threads hijos de un thread de un process de un macroprocess
+     * @param int $idfather
+     * @param int $idProcess
+     * @param int $idMacroprocess
+     * @return mixed
+     */
+    public function select_subthread_associed_thread_associed_process_associed_macroprocess(int $idfather, int $idprocess, int $idmacroprocess)
+    {
+        $this->id = $idfather;
+        $this->idprocess = $idprocess;
+        $this->idmacroprocess = $idmacroprocess;
+        $sql = "SELECT
+                    tbt.*
+                FROM
+                    tb_threads AS tbt
+                    INNER JOIN tb_process AS tbp ON tbp.idProcess = tbt.process_id
+                    INNER JOIN tb_macroprocess AS tbm ON tbm.idMacroprocess = tbp.macroprocess_id
+                WHERE
+                    tbm.mp_status = 'Activo'
+                    AND tbp.p_status = 'Activo'
+                    AND tbt.t_status = 'Activo'
+                    AND tbt.threads_father = ?
+                    AND tbm.idMacroprocess=?
+                    AND tbp.idProcess=?;";
+        $request = $this->select_all($sql, [$this->id, $this->idmacroprocess, $this->idprocess]);
+        return $request;
+    }
 }
