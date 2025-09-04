4440e43cdf88f3759671eb5770953bb70d097cca
yeisondanner <yeisoncarhuapoma@gmail.com>
2025-09-04 08:57:38 -0500

    work dashboard

--- Views/App/Dashboard/dashboard.php
+++ Views/App/Dashboard/dashboard.php
@@ -13,40 +13,38 @@
     </div>
     <div class="row">
         <!-- Card 1 -->
-        <div class="col-md-6 mb-3">
-            <a href="#">
-                <div class="card card-equal card-hover w-100">
-                    <div class="card-body d-flex flex-column">
-                        <div class="d-flex align-items-center mb-3 p-2 rounded">
-                            <div class="icon-circle mr-3"><i class="fa fa-university" aria-hidden="true"></i></div>
-                            <div>
-                                <h5 class="card-title mb-0">Tarjeta Premium</h5>
-                                <small>Glassmorphism Style</small>
-                            </div>
-                        </div>
-                        <p class="card-text">Esta tarjeta tiene un efecto de vidrio esmerilado moderno, elegante y
-                            atractivo para el usuario final. Perfecto para interfaces limpias y minimalistas.</p>
-                    </div>
+        <div class="col-md-4 mb-4">
+            <a href="#" class="card custom-card p-4 text-center h-100">
+                <div class="icon-wrapper bg-primary mx-auto">
+                    <i class="fa fa-wrench"></i>
                 </div>
+                <h5>Cambio de Disco Duro</h5>
+                <p>Se reemplazó disco dañado de 1TB</p>
+                <div class="date"><i class="fa fa-calendar"></i> 2025-09-04</div>
             </a>
         </div>
 
         <!-- Card 2 -->
-        <div class="col-md-6 mb-3">
-            <a href="#">
-                <div class="card card-equal card-hover w-100">
-                    <div class="card-body d-flex flex-column">
-                        <div class="d-flex align-items-center mb-3 p-2 rounded">
-                            <div class="icon-circle mr-3"><i class="fa fa-star" aria-hidden="true"></i></div>
-                            <div>
-                                <h5 class="card-title mb-0">Otra Tarjeta</h5>
-                                <small>Efecto vidrio</small>
-                            </div>
-                        </div>
-                        <p class="card-text">Cuando pasas el mouse, la tarjeta se eleva suavemente y mantiene su
-                            transparencia con el blur de fondo. Efecto elegante que sorprende.</p>
-                    </div>
+        <div class="col-md-4 mb-4">
+            <a href="#" class="card custom-card p-4 text-center h-100">
+                <div class="icon-wrapper bg-success mx-auto">
+                    <i class="fa fa-cogs"></i>
                 </div>
+                <h5>Mantenimiento Preventivo</h5>
+                <p>Limpieza interna y cambio de pasta térmica</p>
+                <div class="date"><i class="fa fa-calendar"></i> 2025-09-01</div>
+            </a>
+        </div>
+
+        <!-- Card 3 -->
+        <div class="col-md-4 mb-4">
+            <a href="#" class="card custom-card p-4 text-center h-100">
+                <div class="icon-wrapper bg-warning mx-auto">
+                    <i class="fa fa-desktop"></i>
+                </div>
+                <h5>Actualización de Software</h5>
+                <p>Se instaló la última versión del sistema operativo</p>
+                <div class="date"><i class="fa fa-calendar"></i> 2025-08-30</div>
             </a>
         </div>
     </div>
